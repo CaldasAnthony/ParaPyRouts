2cf77c1d74427db87004a2c35a3eee890036a70e
CaldasAnthony <33262464+CaldasAnthony@users.noreply.github.com>
2017-11-03 15:11:51 +0100

    Add files via upload

--- pyfunction.py
+++ pyfunction.py
@@ -279,6 +279,7 @@ def ratio(n_species,x_ratio_species_active,IsoComp=False) :
 
 def diag(data_base) :
 
+    print data_base
     file = Dataset("%s.nc"%(data_base))
     variables = file.variables
     controle = variables["controle"][:]
