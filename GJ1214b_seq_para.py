54c8559813e4e720c92a8577a58723e3e7331b5f
CaldasAnthony <33262464+CaldasAnthony@users.noreply.github.com>
2017-11-07 17:01:30 +0100

    Add files via upload

--- GJ1214b_seq_para.py
+++ GJ1214b_seq_para.py
@@ -169,12 +169,12 @@ if Profil == True :
             wh_up = np.where(z_sphe[:,pss-1,:,:] == np.amax(z_sphe))
             z_t = np.amax(z_sphe)
             g_roof = g0*1/(1.+z_t/Rp)**2
-            H_mean = R_gp*T[wh_up[0],pss-1,wh_up[1],wh_up[2]][0]/(M[wh_up[0],pss-1,wh_up[1],wh_up[2]][0]*g_roof)
+            H_mean = R_gp*T[wh_up[0],pss-1,wh_up[1],wh_up[2]][0]/(M_molar[wh_up[0],pss-1,wh_up[1],wh_up[2]][0]*g_roof)
         if TopPressure == 'Down' :
             wh_dn = np.where(z_sphe[:,pss-1,:,:] == np.amin(z_sphe[:,pss-1,:,:]))
             z_t = z_sphe[wh_dn[0],pss-1,wh_dn[1],wh_dn[2]][0]
             g_roof = g0*1/(1.+z_t/Rp)**2
-            H_mean = R_gp*T[wh_dn[0],pss-1,wh_dn[1],wh_dn[2]][0]/(M[wh_dn[0],pss-1,wh_dn[1],wh_dn[2]][0]*g_roof)
+            H_mean = R_gp*T[wh_dn[0],pss-1,wh_dn[1],wh_dn[2]][0]/(M_molar[wh_dn[0],pss-1,wh_dn[1],wh_dn[2]][0]*g_roof)
 
         print "The thickness of the simulation is %i m"%(hmax)
         print "The thickness of the atmosphere is %i m"%(h)
