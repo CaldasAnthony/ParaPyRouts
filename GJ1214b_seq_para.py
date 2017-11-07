dffb6bb490de53216bff6e8785d828e235115f86
CaldasAnthony <33262464+CaldasAnthony@users.noreply.github.com>
2017-11-07 17:06:43 +0100

    Add files via upload

--- GJ1214b_seq_para.py
+++ GJ1214b_seq_para.py
@@ -156,9 +156,7 @@ if Profil == True :
         dim = int(h/delta_z)+2
         M_mean = np.nansum(M_molar[:,pss-1,:,:])/(tss*loss*lass)
         T_mean, T_max, T_min = T_var[0], T_var[1], T_var[2]
-        P_mean = np.nansum(P[:,pss-1,:,:])/(tss*loss*lass)
-        g_roof = g0*1/(1+hmax/Rp)**2
-        H_mean = R_gp*T_mean/(M_mean*g_roof)
+        P_mean = np.exp(np.nansum(np.log(P[:,pss-1,:,:])/(tss*loss*lass)))
 
         if TopPressure == 'Mean' or TopPressure == 'No' :
             M_mean = np.nansum(M_molar[:,pss-1,:,:])/(tss*loss*lass)
