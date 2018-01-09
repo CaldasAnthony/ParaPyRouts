fc221a6d8bdb0c90364b5d1b1e0853141b309b61
CaldasAnthony <33262464+CaldasAnthony@users.noreply.github.com>
2018-01-09 16:22:16 +0100

    Add files via upload

--- GJ1214b_seq_para.py
+++ GJ1214b_seq_para.py
@@ -921,8 +921,9 @@ if Cylindric_transfert_3D == True :
             Q_rmd = np.array([])
         rmind = np.load("%s%s/%s/rmind_%i%i_%s_%i_%i_%i_rmd_%.2f_%.2f_%s.npy"\
                 %(path,name_file,opac_file,reso_long,reso_lat,name_exo,t,dim_bande,x_step,phi_rot,phi_obli,domain))
-        print rmind.size
-        print rmind.size
+        if rank == 0 :
+            print rmind.size
+            print rmind
 
 ########################################################################################################################
     
