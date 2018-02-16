d020184823f0bdd22fca54eac10f46619377454b
CaldasAnthony <33262464+CaldasAnthony@users.noreply.github.com>
2018-02-16 14:52:29 +0100

    Add files via upload

--- GJ1214b_script_para.py
+++ GJ1214b_script_para.py
@@ -114,9 +114,9 @@ ind_cross, ind_active = index_active (n_species,n_species_cross,n_species_active
 #cont_tot = np.array(['H2-He_2011.cia','H2-He_2011.cia','H2O_CONT_SELF.dat','H2O_CONT_FOREIGN.dat'])
 #cont_species = np.array(['H2','He','H2Os','H2O'])
 #cont_associations = np.array(['h2he','h2he','h2oh2o','h2ofor'])
-cont_tot = np.array(['H2-He_2011.cia','H2-He_2011.cia'])
+cont_tot = np.array(['H2-H2_2011.cia','H2-He_2011.cia'])
 cont_species = np.array(['H2','He'])
-cont_associations = np.array(['h2he','h2he'])
+cont_associations = np.array(['h2h2','h2he'])
 
 ########################################################################################################################
 
